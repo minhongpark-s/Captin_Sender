4286ca66c723c0e56e9caeb1e4f7011d7a8cdb53
minhongpark <minhong10003@gmail.com>
2022-12-12 08:01:49 -0800

    221213 robotservice to pub ;

--- static/sender/js/receiveRepeater.js
+++ static/sender/js/receiveRepeater.js
@@ -29,11 +29,11 @@ var statusClient_ = new ROSLIB.Service({
     serviceType : 'std_msgs/string'
 });
 
-var statusClient__ = new ROSLIB.Service({
-  ros : ros,
-  name : 'App_data',
-  serviceType : 'std_msgs/string'
-});
+var toRobot = new ROSLIB.Topic({
+     ros : ros,
+     name : 'Sub_App',
+     messageType : 'std_msgs/string'
+   });
 
 setTimeout(sendRequestToServer('http://3.38.25.123/dashboard/checkDeliveryRequest/'),10000);
 
@@ -127,31 +127,13 @@ function checkLoadedData(){
       //console.log("request method is '" + json.response1[0].method + "'");
       //console.log(typeof(json.response1[0].position))
       if (json.response1[0].status == "found Loaded Data"){
-        // 보내려는 service request의 정보를 등록
-        var send = new ROSLIB.ServiceRequest({
-            //position : pos,
-            input1 : "loaded",
-            input2 : "loaded",
-        });
-        // service 요청을 보냄
-        statusClient_.callService(send, function(result) {
-            console.log('Result for service call on '
-            + statusClient_.name
-            + ': '
-            + result.o);
-            if(result.response == "loadOk"){
-                console.log("ros service response 'ok'")
-                changeLoadedToMovingToGoal()
-                // 마지막 loaded 요청을 처리하면 다시 request를 대기하는 상태로 돌아간다.
-                setTimeout(sendRequestToServer('http://3.38.25.123/dashboard/checkDeliveryRequest/'),1);
-            }
-            // service에서 성공을 반환할 때 까지 무한 반복
-            // 즉 로봇이 데이터를 받아 처리했을 경우
-            else if(result.response == "failed"){
-                console.log("ros service response 'failed'")
-                checkLoadedData()
-            }
+        var datas = new ROSLIB.Message({
+            pubData : "go"
         })
+        toRobot.publish(datas)
+        changeLoadedToMovingToGoal()
+        // 마지막 loaded 요청을 처리하면 다시 request를 대기하는 상태로 돌아간다.
+        setTimeout(sendRequestToServer('http://3.38.25.123/dashboard/checkDeliveryRequest/'),1);
       }
       else if(json.response1[0].status == "no Loaded Data"){
         console.log("no Loaded Data");
